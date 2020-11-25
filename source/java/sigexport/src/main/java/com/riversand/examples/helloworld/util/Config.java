a253b0405689c370d1be28009b2cdeccb58dcd1e
Moulyashreehn <70944660+Moulyashreehn@users.noreply.github.com>
2020-11-25 14:13:15 +0530

    Update Config.java

--- source/java/sigexport/src/main/java/com/riversand/examples/helloworld/util/Config.java
+++ source/java/sigexport/src/main/java/com/riversand/examples/helloworld/util/Config.java
@@ -5,13 +5,13 @@ package com.riversand.examples.helloworld.util;
  */
 public class Config {
 
-    public static final String RDP_URL = System.getenv("RDP_URL");
-    public static final String RDP_PORT = System.getenv("RDP_PORT");
-    public static final String REQUEST_FILE_NAME = System.getenv("REQUEST_FILE_NAME");
-    public static final String TENANT = System.getenv("TENANT");
-    public static final String REQUEST_FILE_PATH = System.getenv("REQUEST_FILE_PATH");
-    public static final String SOURCE_CONFIG_PATH = System.getenv("SOURCE_CONFIG_PATH");
-    public static final String POD_ID = System.getenv("POD_ID");
-    public static final String TASK_ID = System.getenv("TASK_ID");
+    public static final String RDP_URL = System.getenv("manage.rdpengg14.riversand-dataplatform.com");
+    public static final String RDP_PORT = System.getenv("8085");
+    public static final String REQUEST_FILE_NAME = System.getenv("request.json");
+    public static final String TENANT = System.getenv("autoappdev");
+    public static final String REQUEST_FILE_PATH = System.getenv("source/java/");
+    public static final String SOURCE_CONFIG_PATH = System.getenv("batch/job_defs/helloworld/resource_files/");
+    public static final String POD_ID = System.getenv("rdpengg14");
+    public static final String TASK_ID = System.getenv("123");
     public static final String RDP_COMPLETE_URL = "http://" + RDP_URL + ":" + RDP_PORT + "/";
 }
